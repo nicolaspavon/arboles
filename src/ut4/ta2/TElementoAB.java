a63a4a9237b21a64c88ac6c487a127f5deab672d
nicolaspavon <nicolaspavon33@gmail.com>
2019-05-09 19:26:32 -0300

    'hola'

---
+++ src/ut4/ta2/TElementoAB.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut4.ta2;
+
+/**
+ *
+ * @author nico
+ */
+public class TElementoAB<T> implements IElementoAB{
+    
+}
---
+++ src/ut4/ta2/UT4TA2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut4.ta2;
+
+/**
+ *
+ * @author nico
+ */
+public class UT4TA2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
